59ce372b55b90e9667846026a30c4b5128ab8749
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2022-08-19 10:08:32 -0500

    Marking vercel as complete as we get ready to deploy it to vercel

--- pages/api/platforms.ts
+++ pages/api/platforms.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json([
-        { name: "Vercel", isComplete: false },
+        { name: "Vercel", isComplete: true },
         { name: "Heroku", isComplete: false },
         { name: "DigitalOcean", isComplete: false },
         { name: "AWS", isComplete: false }

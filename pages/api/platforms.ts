0b678285aee6333da5ca903dd66d1ed8da7a8e25
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2022-08-23 10:58:35 -0500

    Deploying to, and marking Heroku as complete

--- pages/api/platforms.ts
+++ pages/api/platforms.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json([
         { name: "Vercel", isComplete: true },
-        { name: "Heroku", isComplete: false },
+        { name: "Heroku", isComplete: true },
         { name: "DigitalOcean", isComplete: false },
         { name: "AWS", isComplete: false }
     ]);
